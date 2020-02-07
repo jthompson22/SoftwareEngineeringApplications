706ab9e9c8dfea104286898bffa585f6e64882f6
Axel Ward <35614781+AxelWard@users.noreply.github.com>
2020-02-07 15:14:42 -0700

    Zero Feature Release

--- src/Code_Library/Server/RiverNet/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import WaterQuantity
-
-# Register your models here.
-
-@admin.register(WaterQuantity)
-
-class WaterQuantityAdmin(admin.ModelAdmin):
-    list_display = ['river_name', 'enteree']
--- src/Code_Library/Server/RiverNet/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class WaterQuantity(models.Model):
-    river_name = models.CharField(max_length=100)
-    enteree = models.CharField(max_length=100)
