e5d48dd83d66ec0a085b1cfad0ca12b1c61494b5
Axel Ward <35614781+AxelWard@users.noreply.github.com>
2020-03-05 21:13:47 -0700

    Update testConfig.py

--- test/testConfig.py
+++ test/testConfig.py
@@ -1,6 +1,6 @@
 import sys 
 import os
-sys.path.append(os.path.abspath("SoftwareEngineeringApplications/Code Library/venv/polls/apps"))
+sys.path.append(os.path.abspath("../Code Library/venv/polls/apps"))
 
 from apps import *
 
