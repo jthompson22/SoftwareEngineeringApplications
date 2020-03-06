2d56668ed48ed89da0f9b6a63d445eb1b98a4b01
Axel Ward <35614781+AxelWard@users.noreply.github.com>
2020-03-05 20:48:01 -0700

    Test Fix

--- test/testConfig.py
+++ test/testConfig.py
@@ -1,4 +1,8 @@
-import ../"Code Library"/venv/polls/apps.py
+import sys 
+import os
+sys.path.append(os.path.abspath("../Code Library/venv/polls/apps"))
+
+import 
 
 def test_config():
     a = new PollsConfig()
