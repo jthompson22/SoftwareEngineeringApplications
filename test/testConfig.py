6718bddf0967b91511741149f40b2f92e674783a
Axel Ward <35614781+AxelWard@users.noreply.github.com>
2020-03-05 21:29:57 -0700

    Update testConfig.py

--- test/testConfig.py
+++ test/testConfig.py
@@ -1,6 +1,8 @@
 import sys 
 import os
-import os.path.abspath("./Code Library/venv/polls/apps")
+sys.path.append(os.path.abspath("./Code Library/venv/polls/apps"))
+
+from apps import PollsConfig
 
 def test_config():
     a = PollsConfig()
