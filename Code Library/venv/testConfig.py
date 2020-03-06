6bb01920277a8bf742c729cbdf1e6e66b1ba11dc
Axel Ward <35614781+AxelWard@users.noreply.github.com>
2020-03-05 22:33:35 -0700

    Update testConfig.py

--- Code Library/venv/testConfig.py
+++ Code Library/venv/testConfig.py
@@ -1,11 +1,6 @@
 import sys 
 import os
-from django.apps import AppConfig
-
-sys.path.append(os.path.abspath('/polls/'))
-
-from polls import apps
+import django
 
 def test_config():
-    a = apps.PollsConfig('pollsTest', 'testModule')
-    assert a.name == 'polls'
+    assert django in sys.modules
